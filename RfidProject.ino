95acb53b0bdeeb8248bd2be8f63c126eae7f32d5
Evy Svensson <evymail87@gmail.com>
2020-05-05 14:04:55 +0200

    Add project files.

---
+++ RfidProject.ino
@@ -0,0 +1,20 @@
+
+#include <dummy.h>
+#include"RfidFunctions.h";
+
+
+ServiceDesk myFrontDesk;
+
+
+void setup()
+{
+    myFrontDesk.setup();
+    
+  
+}
+
+void loop(void) {
+    delay(150);
+    myFrontDesk.handleRFID();
+  
+}
